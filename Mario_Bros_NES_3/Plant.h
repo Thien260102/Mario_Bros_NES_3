ef9743308584d837df72d7a2a8d4522d7e242885
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-21 10:34:07 +0700

    set IsCollidable = 1 and solve collision between Plant and Koopas

--- Mario_Bros_NES_3/Plant.h
+++ Mario_Bros_NES_3/Plant.h
@@ -50,12 +50,12 @@ protected:
 	virtual void Render();
 
 
-	virtual int IsCollidable() { return 0; }
+	virtual int IsCollidable() { return 1; }
 	virtual int IsBlocking() { return 0; }
 
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void OnNoCollision(DWORD dt) {}
-	virtual void OnCollisionWith(LPCOLLISIONEVENT e) {}
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
 	int GetAniId();
 public:
 	CPlant(float x, float y, int type) :CGameObject(x, y)
