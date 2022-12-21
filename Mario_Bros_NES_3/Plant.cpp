3cccef62e265cecdfdd5f45d50203788548f752b
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-21 19:57:01 +0700

    add collision between Plant and Mario Raccoon's tail

--- Mario_Bros_NES_3/Plant.cpp
+++ Mario_Bros_NES_3/Plant.cpp
@@ -184,4 +184,10 @@ void CPlant::OnCollisionWith(LPCOLLISIONEVENT e)
 			koopas->SetState(KOOPAS_STATE_DIE);
 		}
 	}
+	else if (dynamic_cast<CPhaseChecker*>(e->obj))
+	{
+		CPhaseChecker* phaseChecker = dynamic_cast<CPhaseChecker*>(e->obj);
+		if (phaseChecker->GetType() == PHASECHECK_BY_MARIO)
+			this->Delete();
+	}
 }
