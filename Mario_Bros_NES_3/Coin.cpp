2621573e54de534f0983faf5340c35db629bedd0
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-23 14:29:17 +0700

    Add new macros to make coin appear when Mario jump into Brick from below. Add and update some method to solve behavior of Coin

--- Mario_Bros_NES_3/Coin.cpp
+++ Mario_Bros_NES_3/Coin.cpp
@@ -1,5 +1,20 @@
 #include "Coin.h"
 
+void CCoin::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	if (vy == 0 && ay == 0)
+		return;
+
+	if (ay != 0 && vy >= 0)
+		this->Delete();
+
+	if (ay != 0)
+	{
+		vy += ay * dt;
+		y += vy * dt;
+	}
+}
+
 void CCoin::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
@@ -14,4 +29,10 @@ void CCoin::GetBoundingBox(float& l, float& t, float& r, float& b)
 	t = y - COIN_BBOX_HEIGHT / 2;
 	r = l + COIN_BBOX_WIDTH;
 	b = t + COIN_BBOX_HEIGHT;
+}
+
+void CCoin::Deflected(int direction)
+{
+	ay = COIN_GRAVITY;
+	vy = -COIN_DEFLECT;
 }
--- Mario_Bros_NES_3/Coin.h
+++ Mario_Bros_NES_3/Coin.h
@@ -4,6 +4,10 @@
 #include "Animation.h"
 #include "Animations.h"
 
+
+#define COIN_DEFLECT  0.4f //use when Mario jump into Brick from below
+#define COIN_GRAVITY 0.001f //use when Mario jump into Brick from below
+
 #define ID_ANI_COIN 11000
 
 //#define	COIN_WIDTH 10
@@ -11,10 +15,13 @@
 #define COIN_BBOX_HEIGHT 15
 
 class CCoin : public CGameObject {
+private:
+	float ay;
 public:
-	CCoin(float x, float y) : CGameObject(x, y) {}
+	CCoin(float x, float y) : CGameObject(x, y) { ay = 0; vy = 0; }
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return 0; }
+	void Deflected(int direction);
 };
