c46606ac1fc1b733d58985c4b1e926c50186f9b3
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-24 16:03:02 +0700

    add new macros for determine type of platform. Update IsBlocking to solve collision between Mario (or Koopas, goomba) and platform.

--- Mario_Bros_NES_3/Platform.h
+++ Mario_Bros_NES_3/Platform.h
@@ -5,6 +5,10 @@
 // 
 // The most popular type of object in Mario! 
 // 
+#define PLATFORM_TYPE_BLOCK 10
+#define PLATFORM_TYPE_NORMAL 20
+#define PLATFORM_TYPE_NOCOLLISION 30
+
 class CPlatform : public CGameObject
 {
 protected: 
@@ -13,10 +17,20 @@ protected:
 	float cellHeight;
 	int spriteIdBegin, spriteIdMiddle, spriteIdEnd;
 
+	int _type;
+	virtual int IsBlocking() 
+	{ 
+		if (_type == PLATFORM_TYPE_BLOCK)
+			return 1;
+		else if (_type == PLATFORM_TYPE_NORMAL)
+			return 2;
+		return 0;
+	}
+
 public: 
 	CPlatform(float x, float y,
 		float cell_width, float cell_height, int length,
-		int sprite_id_begin, int sprite_id_middle, int sprite_id_end) :CGameObject(x, y)
+		int sprite_id_begin, int sprite_id_middle, int sprite_id_end, int type) :CGameObject(x, y)
 	{
 		this->length = length;
 		this->cellWidth = cell_width;
@@ -24,12 +38,15 @@ public:
 		this->spriteIdBegin = sprite_id_begin;
 		this->spriteIdMiddle = sprite_id_middle;
 		this->spriteIdEnd = sprite_id_end;
+		_type = type;
 	}
 
 	void Render();
 	void Update(DWORD dt) {}
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	void RenderBoundingBox();
+	
+	int GetType() { return _type; }
 };
 
 typedef CPlatform* LPPLATFORM;
