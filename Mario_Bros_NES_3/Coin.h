f82c03e4904b15048c597e5118d4b23658967a18
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-28 13:49:39 +0700

    new some macros( to determine Coin was transformed by what, and time exist), new variables (type, remainTime_start (determine when Coin transform to Brick)) and CoinTransformBrick

--- Mario_Bros_NES_3/Coin.h
+++ Mario_Bros_NES_3/Coin.h
@@ -15,11 +15,28 @@
 #define COIN_BBOX_WIDTH 15
 #define COIN_BBOX_HEIGHT 15
 
+#define COIN_BY_NONE 0
+#define COIN_TRANSFORMED_FROM_BRICK 1
+
+#define COIN_TIMEOUT 10000
+
 class CCoin : public CGameObject {
 private:
 	float ay;
+	int _type;
+	ULONGLONG remainTime_start;
+
+	void CoinTransformBrick();
 public:
-	CCoin(float x, float y) : CGameObject(x, y) { ay = 0; vy = 0; }
+	CCoin(float x, float y, int type = 0) : CGameObject(x, y) 
+	{ 
+		ay = 0; 
+		vy = 0; 
+		_type = type;
+		remainTime_start = 0;
+		if (_type == COIN_TRANSFORMED_FROM_BRICK)
+			remainTime_start = GetTickCount64();
+	}
 	void Render();
 	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
