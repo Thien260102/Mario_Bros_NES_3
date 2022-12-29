789a305146a35371dd903c5a0aef1e5014bb1dd0
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-29 12:10:25 +0700

    adjust macros, add new variables to handle behavior of Mario's tail attack and update some method

--- Mario_Bros_NES_3/PhaseChecker.h
+++ Mario_Bros_NES_3/PhaseChecker.h
@@ -10,11 +10,13 @@
 
 
 #define PHASECHECK_GRAVITY 0.001f
-#define PHASECHECK_ATTACK_SPEED	1.4f
+#define PHASECHECK_ATTACK_SPEED	1.5f
 #define PHASECHECK_ATTACK_RANGE 3.0f
 
 #define PHASECHECK_BY_KOOPAS 10
 #define PHASECHECK_BY_MARIO 20
+
+#define PHASECHECK_ATTACK_TIME 300
 
 class CKoopas;
 
@@ -25,11 +27,14 @@ protected:
 	int height;
 
 	float old_x;
+	int isAttackedFront;
+	int isAttackBehind;
 
 	int _type;
 
 	float ax;
 	float ay;
+	ULONGLONG attack_start;
 
 	virtual int IsCollidable() { return 1; }
 	virtual int IsBlocking() { return 0; }
@@ -37,6 +42,7 @@ protected:
 
 	virtual void OnNoCollision(DWORD dt);
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+	virtual void OnCollisionWith(LPGAMEOBJECT o);
 
 	void OnCollisionWithBrick(LPCOLLISIONEVENT e);
 	void OnCollisionWithGoomba(LPCOLLISIONEVENT e);
@@ -61,12 +67,24 @@ public:
 		height = Height;
 		_type = type;
 		old_x = x;
+		attack_start = 0;
+		isAttackedFront = 0;
+		isAttackBehind = 0;
 	}
 
 	void Attack(int direction) 
 	{ 
-		vx += PHASECHECK_ATTACK_SPEED * direction; 
+		vx += PHASECHECK_ATTACK_SPEED * direction;
 		old_x = x;
+		isAttackedFront = 0; // only attack 1 gameobject in front of Mario
+		isAttackBehind = 0;  // only attack 1 gameobject(except Block, Platform) behide Mario
+	}
+
+	bool IsAttacking() { return attack_start != 0; }
+
+	void SetAttackTime(ULONGLONG time) 
+	{ 
+		attack_start = time; 
 	}
 
 	int GetType() { return _type; }
