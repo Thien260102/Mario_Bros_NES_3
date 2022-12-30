64f7052d13b4b128b1f87d860f637d9ce148af1e
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-30 12:16:14 +0700

    just a sensor of Koopas

--- Mario_Bros_NES_3/PhaseChecker.cpp
+++ Mario_Bros_NES_3/PhaseChecker.cpp
@@ -17,43 +17,6 @@ void CPhaseChecker::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 	vy += ay * dt;
 	vx += ax * dt;
 
-
-	/*if (_type == PHASECHECK_BY_MARIO && x != old_x)
-		DebugOut(L"x: %f, old_x: %f\n", x, old_x);*/
-
-	if (attack_start != 0)
-	{
-		/*if (vy == ay * dt)
-			vy = 0;*/
-		if ((GetTickCount64() - attack_start > PHASECHECK_ATTACK_TIME))
-		{
-			vx = 0;
-			vy = 0;
-		}
-		// use this case if tail of Mario over the attack range
-		else if (_type == PHASECHECK_BY_MARIO && abs(x + vx * dt - old_x) > PHASECHECK_ATTACK_RANGE * width)
-		{
-			if (vx >= 0)
-			{
-				vx = (PHASECHECK_ATTACK_RANGE * width + old_x - x) / dt;
-			}
-			else
-			{
-				vx = (old_x - PHASECHECK_ATTACK_RANGE * width - x) / dt;
-			}
-		}
-		else if (isAttackedBehind == false && ((GetTickCount64() - attack_start) < PHASECHECK_ATTACK_TIME / 4))
-		{
-			CCollision::GetInstance()->Process(this, coObjects);
-		}
-
-
-		if (isAttackedFront == false)
-			CCollision::GetInstance()->Process(this, dt, coObjects);
-
-		return;
-	}
-
 	CGameObject::Update(dt, coObjects);
 	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
@@ -64,74 +27,6 @@ void CPhaseChecker::OnCollisionWith(LPCOLLISIONEVENT e)
 	{
 		vy = 0;
 	}
-	if (_type == PHASECHECK_BY_KOOPAS || e->nx == 0 || dynamic_cast<CPlatform*>(e->obj))
-		return;
-
-	isAttackedFront = true;
-	if (dynamic_cast<CBrick*>(e->obj))
-		OnCollisionWithBrick(e);
-	else if (dynamic_cast<CGoomba*>(e->obj))
-		OnCollisionWithGoomba(e);
-	else if (dynamic_cast<CPlant*>(e->obj))
-		OnCollisionWithPlant(e);
-	else if ((CKoopas*)(e->obj))
-		OnCollisionWithKoopas(e);
-	else
-		isAttackedFront = false;
-}
-
-
-void CPhaseChecker::OnCollisionWithBrick(LPCOLLISIONEVENT e)
-{
-	CBrick* brick = dynamic_cast<CBrick*>(e->obj);
-
-	switch (brick->GetType())
-	{
-	case BRICK_TYPE_GOLD:
-		brick->SetType(BRICK_TYPE_BREAK);
-		break;
-
-	case BRICK_TYPE_QUESTION:
-		brick->SetType(BRICK_TYPE_EMPTY);
-		break;
-	}
-	
-	isAttackedFront = PHASECHECK_COLLIDED_BRICK;
-}
-
-void CPhaseChecker::OnCollisionWithGoomba(LPCOLLISIONEVENT e)
-{
-	CGoomba* goomba = dynamic_cast<CGoomba*>(e->obj);
-	if (goomba->GetState() == GOOMBA_STATE_DIE_1 || goomba->GetState() == GOOMBA_STATE_DIE_2)
-		return;
-
-	goomba->SetState(GOOMBA_STATE_DIE_2);
-	if (e->nx > 0)
-		goomba->Deflected(DEFLECT_DIRECTION_LEFT);
-	else
-		goomba->Deflected(DEFLECT_DIRECTION_RIGHT);
-
-	isAttackedFront = PHASECHECK_COLLIDED_GOOMBA;
-}
-
-void CPhaseChecker::OnCollisionWithKoopas(LPCOLLISIONEVENT e)
-{
-	e->obj->SetState(200); // KOOPAS_STATE_SHELL 200
-
-	float kx, ky;
-	e->obj->GetPosition(kx, ky);
-	if (kx <= x)
-		e->obj->Deflected(DEFLECT_DIRECTION_LEFT);
-	else
-		e->obj->Deflected(DEFLECT_DIRECTION_RIGHT);
-
-	isAttackedFront = PHASECHECK_COLLIDED_KOOPAS;
-}
-
-void CPhaseChecker::OnCollisionWithPlant(LPCOLLISIONEVENT e)
-{
-	e->obj->Delete();
-	isAttackedFront = PHASECHECK_COLLIDED_PLANT;
 }
 
 void CPhaseChecker::OnNoCollision(DWORD dt)
@@ -141,59 +36,7 @@ void CPhaseChecker::OnNoCollision(DWORD dt)
 }
 
 
-void CPhaseChecker::OnCollisionWith(LPGAMEOBJECT obj)
-{
-	isAttackedBehind = true;
-
-	if (dynamic_cast<CGoomba*>(obj))
-	{
-		CGoomba* goomba = dynamic_cast<CGoomba*>(obj);
-		if (goomba->GetState() == GOOMBA_STATE_DIE_1 || goomba->GetState() == GOOMBA_STATE_DIE_2)
-			return;
-		DebugOut(L"Hello goomba\n");
-		goomba->SetState(GOOMBA_STATE_DIE_2);
-		float gx, gy;
-		obj->GetPosition(gx, gy);
-		if (gx < x)
-			goomba->Deflected(DEFLECT_DIRECTION_LEFT);
-		else
-			goomba->Deflected(DEFLECT_DIRECTION_RIGHT);
-	}
-	else if (dynamic_cast<CPlant*>(obj))
-	{
-		DebugOut(L"Hello plant\n");
-		obj->Delete();
-	}
-	else if ((CKoopas*)obj)
-	{
-		obj->SetState(200); // KOOPAS_STATE_SHELL 200
-		DebugOut(L"Hello koopas\n");
-		float kx, ky;
-		obj->GetPosition(kx, ky);
-		if (kx <= x)
-			obj->Deflected(DEFLECT_DIRECTION_LEFT);
-		else
-			obj->Deflected(DEFLECT_DIRECTION_RIGHT);
-	}
-	else
-		isAttackedBehind = false;
-}
-
 void CPhaseChecker::Render()
 {
 	RenderBoundingBox();
-
-	CAnimations* animations = CAnimations::GetInstance();
-	if (attack_start != 0)
-	{
-		if (isAttackedBehind && (GetTickCount64() - attack_start) < PHASECHECK_ATTACK_TIME / 2)
-		{
-			animations->Get(ID_ANI_TAIL_MARIO_ATTACKED_ENEMIES)->Render(x, y);
-		}
-		else if (isAttackedFront && isAttackedFront != PHASECHECK_COLLIDED_BRICK
-			&& (GetTickCount64() - attack_start) > PHASECHECK_ATTACK_TIME / 2)
-		{
-			animations->Get(ID_ANI_TAIL_MARIO_ATTACKED_ENEMIES)->Render(x + nx * PHASECHECK_ATTACK_RANGE * width, y);
-		}
-	}
 }
--- Mario_Bros_NES_3/PhaseChecker.h
+++ Mario_Bros_NES_3/PhaseChecker.h
@@ -1,28 +1,9 @@
 #pragma once
-#ifndef CPHASECHECKER_H
-#define CPHASECHECKER_H
-
-
 #include "GameObject.h"
-#include "Goomba.h"
-#include "Brick.h"
-#include "Plant.h"
 
 #define PHASECHECK_GRAVITY 0.001f
-#define PHASECHECK_ATTACK_SPEED	1.5f
-#define PHASECHECK_ATTACK_RANGE 3.0f
-
-#define PHASECHECK_BY_KOOPAS 10
-#define PHASECHECK_BY_MARIO 20
 
 #define PHASECHECK_ATTACK_TIME 300
-
-#define PHASECHECK_COLLIDED_BRICK 2
-#define PHASECHECK_COLLIDED_GOOMBA 3
-#define PHASECHECK_COLLIDED_KOOPAS 4
-#define PHASECHECK_COLLIDED_PLANT 5
-
-class CKoopas;
 
 class CPhaseChecker : public CGameObject
 {
@@ -30,15 +11,8 @@ protected:
 	int width;
 	int height;
 
-	float old_x;
-	int isAttackedFront;
-	int isAttackedBehind;
-
-	int _type;
-
 	float ax;
 	float ay;
-	ULONGLONG attack_start;
 
 	virtual int IsCollidable() { return 1; }
 	virtual int IsBlocking() { return 0; }
@@ -46,12 +20,6 @@ protected:
 
 	virtual void OnNoCollision(DWORD dt);
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
-	virtual void OnCollisionWith(LPGAMEOBJECT o);
-
-	void OnCollisionWithBrick(LPCOLLISIONEVENT e);
-	void OnCollisionWithGoomba(LPCOLLISIONEVENT e);
-	void OnCollisionWithKoopas(LPCOLLISIONEVENT e);
-	void OnCollisionWithPlant(LPCOLLISIONEVENT e);
 
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 
@@ -60,39 +28,13 @@ protected:
 	
 public:
 	//CPhaseCheck() : CGameObject() {}
-	CPhaseChecker(float x, float y, int Width, int Height, int type) : CGameObject(x, y)
+	CPhaseChecker(float x, float y, int Width, int Height) : CGameObject(x, y)
 	{
 		this->ax = 0;
 		this->ay = PHASECHECK_GRAVITY;
 		vy = 0;
 		width = Width;
 		height = Height;
-		_type = type;
-		old_x = x;
-		attack_start = 0;
-		isAttackedFront = 0;
-		isAttackedBehind = 0;
 	}
-
-	void Attack(int direction) 
-	{ 
-		vx += PHASECHECK_ATTACK_SPEED * direction;
-		old_x = x;
-		isAttackedFront = 0; // only attack 1 gameobject in front of Mario
-		isAttackedBehind = 0;  // only attack 1 gameobject(except Block, Platform) behide Mario
-
-		nx = direction;
-	}
-
-	bool IsAttacking() { return attack_start != 0; }
-
-	void SetAttackTime(ULONGLONG time) 
-	{ 
-		attack_start = time; 
-	}
-
-	int GetType() { return _type; }
 
 };
-
-#endif // !CPHASECHECKER_H
