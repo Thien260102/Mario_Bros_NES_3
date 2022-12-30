fec22885ff0fcd7fb4ab6c13f4319cf285ba5ac4
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-30 13:53:28 +0700

    add new macros and variables, methods to make like real game

--- Mario_Bros_NES_3/Portal.h
+++ Mario_Bros_NES_3/Portal.h
@@ -2,6 +2,18 @@
 
 #include "GameObject.h"
 
+#define GIFT_FLYING_SPEED 0.02f
+
+#define GIFT_TYPE_MUSHROOM	1
+#define GIFT_TYPE_FLOWER	2
+#define GIFT_TYPE_STAR		3
+
+#define ID_ANI_GIFT_MUSHROOM	11100
+#define ID_ANI_GIFT_FLOWER		11101
+#define ID_ANI_GIFT_STAR		11102
+
+#define SWITCH_GIFT_TIME 100
+#define SWITCHING_SCENE_TIME 2000
 /*
 	Object that triggers scene switching
 */
@@ -9,8 +21,15 @@ class CPortal : public CGameObject
 {
 	int scene_id;	// target scene to switch to 
 
+	int giftType;
+
+	ULONGLONG switchGift_start;
+	ULONGLONG switchScene_start;
+
 	float width;
 	float height; 
+
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 public:
 	CPortal(float l, float t, float r, float b, int scene_id);
 	virtual void Render();
@@ -20,4 +39,7 @@ public:
 	
 	int GetSceneId() { return scene_id;  }
 	int IsBlocking() { return 0; }
+
+	void SwitchScene();
+	int IsSwitchingScene() { return switchScene_start; }
 };
