8ced1850297e69bbad551c840bfec797f66ea832
Thien260102 <nguyenvanthien18209@gmail.com>
2022-12-30 13:54:01 +0700

    solve Gift and SwitchScene

--- Mario_Bros_NES_3/Portal.cpp
+++ Mario_Bros_NES_3/Portal.cpp
@@ -1,6 +1,7 @@
 #include "Portal.h"
 #include "Game.h"
 #include "Textures.h"
+#include "AssetIDs.h"
 
 CPortal::CPortal(float l, float t, float r, float b, int scene_id )
 {
@@ -9,6 +10,10 @@ CPortal::CPortal(float l, float t, float r, float b, int scene_id )
 	y = t;
 	width = r - l;
 	height = b - t;
+	giftType = GIFT_TYPE_MUSHROOM;
+	switchScene_start = 0;
+	switchGift_start = GetTickCount64();
+	vy = 0;
 }
 
 void CPortal::RenderBoundingBox()
@@ -32,9 +37,79 @@ void CPortal::RenderBoundingBox()
 	CGame::GetInstance()->Draw(x - cx, y - cy, bbox, nullptr, BBOX_ALPHA, rect.right - 1, rect.bottom - 1);
 }
 
+void CPortal::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	y += vy * dt;
+
+	//switching gift
+	if (GetTickCount64() - switchGift_start > SWITCH_GIFT_TIME)
+	{
+		switchGift_start = GetTickCount64();
+		if (switchScene_start == 0)
+		{
+			switch (giftType)
+			{
+			case GIFT_TYPE_MUSHROOM:
+				giftType = GIFT_TYPE_FLOWER;
+				break;
+
+			case GIFT_TYPE_FLOWER:
+				giftType = GIFT_TYPE_STAR;
+				break;
+
+			case GIFT_TYPE_STAR:
+				giftType = GIFT_TYPE_MUSHROOM;
+				break;
+			}
+		}
+	}
+	
+	if(switchScene_start != 0)
+		SwitchScene();
+}
+
 void CPortal::Render()
 {
 	RenderBoundingBox();
+
+	if (switchScene_start == 0)
+	{
+		CSprites* sprites = CSprites::GetInstance();
+
+		switch (giftType)
+		{
+		case GIFT_TYPE_MUSHROOM:
+			sprites->Get(ID_SPRITE_GIFT_MUSHROOM)->Draw(x, y);
+			break;
+
+		case GIFT_TYPE_FLOWER:
+			sprites->Get(ID_SPRITE_GIFT_FLOWER)->Draw(x, y);
+			break;
+
+		case GIFT_TYPE_STAR:
+			sprites->Get(ID_SPRITE_GIFT_STAR)->Draw(x, y);
+			break;
+		}
+	}
+	else
+	{
+		CAnimations* animations = CAnimations::GetInstance();
+
+		switch (giftType)
+		{
+		case GIFT_TYPE_MUSHROOM:
+			animations->Get(ID_ANI_GIFT_MUSHROOM)->Render(x, y);
+			break;
+
+		case GIFT_TYPE_FLOWER:
+			animations->Get(ID_ANI_GIFT_FLOWER)->Render(x, y);
+			break;
+
+		case GIFT_TYPE_STAR:
+			animations->Get(ID_ANI_GIFT_STAR)->Render(x, y);
+			break;
+		}
+	}
 }
 
 void CPortal::GetBoundingBox(float &l, float &t, float &r, float &b)
@@ -43,4 +118,20 @@ void CPortal::GetBoundingBox(float &l, float &t, float &r, float &b)
 	t = y - height/2;
 	r = x + width/2;
 	b = y + height/2;
+}
+
+void CPortal::SwitchScene()
+{
+	if (switchScene_start == 0)
+	{
+		switchScene_start = GetTickCount64();
+		vy = -GIFT_FLYING_SPEED;
+	}
+	else
+		if (GetTickCount64() - switchScene_start > SWITCHING_SCENE_TIME)
+		{
+			switchScene_start = 0;
+			vy = 0;
+			CGame::GetInstance()->InitiateSwitchScene(scene_id);
+		}
 }
