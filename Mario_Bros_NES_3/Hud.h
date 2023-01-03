aa4216c2cb11b79087a8efa9b4cb6bf86549ba72
Thien260102 <nguyenvanthien18209@gmail.com>
2023-01-03 08:34:30 +0700

    add some macros to solve some method and Score of some things (example: Super Mushroom or LEAF (1000), Coin (100), ...) new variable instance to use for easy some methods to collect Score and Gift, Coin

--- Mario_Bros_NES_3/Hud.h
+++ Mario_Bros_NES_3/Hud.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "GameObject.h"
+#include "Mario.h"
 
-#define ID_ANI_P_CHANGING_COLOR	8888
+#define SPEED_LEVEL	0.016f // (MARIO_RUNNING_SPEED - MARIO_WALKING_SPEED) / 6
+
+#define ID_ANI_P_CHANGING_COLOR	11111
 #define EFFECTS_SPEED 0.02f
 
 #define HUD_BBOX_WIDTH	153
@@ -13,8 +16,12 @@
 
 #define NUMBER_WIDTH	7
 #define NUMBER_HEIGHT	6
+
 #define MARIO_SYMBOL_WIDTH	15
 #define MARIO_SYMBOL_HEIGHT	7
+
+#define ARROW_SYMBOL_WIDTH	7
+#define POWER_SYMBOL_WIDTH	14
 
 ////	relative location to HUD	////
 #define ROW_ABOVE_Y 8	// contain something example: World 1.1 , >>>>> P and $ 12
@@ -36,9 +43,15 @@
 
 #define ADJUST 3
 
+#define SCORE_SUPER_MUSHROOM_LEAF	1000
+#define SCORE_COIN					100
+#define SCORE_MARIO_JUMP_ON_ENEMIES	100
+#define SCORE_TAIL_ATTACKED_ENEMIES	100
+
 class CHud : public CGameObject
 {
 protected:
+	static CHud* _instance;
 
 	int coin;
 	int life;
@@ -54,8 +67,15 @@ public:
 	{
 		gifts.clear();
 		coin = 0;
-		life = 39;
-		score = 30000;
+		life = 1;
+		score = 0;
+	}
+
+	static CHud* GetInstance()
+	{
+		if (_instance == NULL)
+			_instance = new CHud(0, 0);
+		return _instance;
 	}
 
 	void GetBoundingBox(float& left, float& top, float& right, float& bottom);
@@ -69,10 +89,16 @@ public:
 
 	virtual int IsBlocking() { return 0; }
 
-	void CollectCoin() { coin++; }
+	void CollectCoin() 
+	{ 
+		coin++; 
+		score += SCORE_COIN;
+	}
+
 	void Collect_1upMushroom() { life++; }
 	void MarioDeath() { life--; }
-	int AddScore(int Score) { score = Score; }
+	void CollectScore(int Score) { score += Score;}
+	void CollectGift(int gift) { gifts.push_back(gift); }
 
 	int CanRevive() { return life > 0; }
 };
