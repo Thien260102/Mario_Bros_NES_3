764c12cf711c58bd7d7f778c1f7b5f6551cb3446
Thien260102 <nguyenvanthien18209@gmail.com>
2023-01-05 14:22:13 +0700

    Handle key in worldmap

--- Mario_Bros_NES_3/WorldMapKeyEventHandler.cpp
+++ Mario_Bros_NES_3/WorldMapKeyEventHandler.cpp
@@ -3,29 +3,55 @@
 #include "debug.h"
 #include "Game.h"
 
-#include "Mario.h"
+#include "Mario_WorldMap.h"
 #include "WorldMapScene.h"
 
-//void CWorldMapKeyEventHandler::OnKeyDown(int KeyCode)
-//{
-//	//DebugOut(L"[INFO] KeyDown: %d\n", KeyCode);
-//	//CMario* mario = (CMario*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
-//	//LPGAME game = CGame::GetInstance();
-//
-//}
-//
-//void CWorldMapKeyEventHandler::OnKeyUp(int KeyCode)
-//{
-//	//DebugOut(L"[INFO] KeyUp: %d\n", KeyCode);
-//
-//	//CMario* mario = (CMario*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
-//	
-//}
-//
-//void CWorldMapKeyEventHandler::KeyState(BYTE* states)
-//{
-//	//LPGAME game = CGame::GetInstance();
-//	//CMario* mario = (CMario*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
-//
-//}
-;
+void CWorldMapKeyEventHandler::OnKeyDown(int KeyCode)
+{
+	//DebugOut(L"[INFO] KeyDown: %d\n", KeyCode);
+	CMario_WorldMap* mario = (CMario_WorldMap*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	LPGAME game = CGame::GetInstance();
+	switch (KeyCode)
+	{
+	case DIK_S:
+		mario->SetCanGoIntoPortal(1);
+		break;
+	}
+
+}
+
+void CWorldMapKeyEventHandler::OnKeyUp(int KeyCode)
+{
+	//DebugOut(L"[INFO] KeyUp: %d\n", KeyCode);
+
+	CMario_WorldMap* mario = (CMario_WorldMap*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	switch (KeyCode)
+	{
+	case DIK_S:
+		mario->SetCanGoIntoPortal(0);
+		break;
+	}
+}
+
+void CWorldMapKeyEventHandler::KeyState(BYTE* states)
+{
+	CMario_WorldMap* mario = (CMario_WorldMap*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	LPGAME game = CGame::GetInstance();
+	if (game->IsKeyDown(DIK_RIGHT))
+	{
+		mario->SetState(MARIO_WM_STATE_WALKING_RIGHT);
+	}
+	else if (game->IsKeyDown(DIK_LEFT))
+	{
+		mario->SetState(MARIO_WM_STATE_WALKING_LEFT);
+	}
+	else if (game->IsKeyDown(DIK_UP))
+	{
+		mario->SetState(MARIO_WM_STATE_WALKING_UP);
+	}
+	else if (game->IsKeyDown(DIK_DOWN))
+	{
+		mario->SetState(MARIO_WM_STATE_WALKING_DOWN);
+	}
+
+}
--- Mario_Bros_NES_3/WorldMapKeyEventHandler.h
+++ Mario_Bros_NES_3/WorldMapKeyEventHandler.h
@@ -4,9 +4,9 @@
 class CWorldMapKeyEventHandler : public CSceneKeyHandler
 {
 public:
-	virtual void KeyState(BYTE* states) {};
-	virtual void OnKeyDown(int KeyCode) {};
-	virtual void OnKeyUp(int KeyCode) {};
+	virtual void KeyState(BYTE* states);
+	virtual void OnKeyDown(int KeyCode);
+	virtual void OnKeyUp(int KeyCode);
 	CWorldMapKeyEventHandler(LPSCENE s) :CSceneKeyHandler(s) {};
 };
 
